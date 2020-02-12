1db87f9e403ea0efd174c007f783bb5dcd1b85ea
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 01:31:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,6 +18,34 @@ def find_max_value(array)
     return x
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def find_min_value(array)
-  array.min
+  x = 0 
+  array.length.times do |y|
+    if x > array[y]
+      x = array[y]
+    end  
+  end  
+  x
 end
